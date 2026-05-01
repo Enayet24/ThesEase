05eac9d33b9f8ae4955f77c69eff632aa78f1099
Enayet Ali <126701907+Enayet24@users.noreply.github.com>
2026-05-01 21:06:39 +0600

    Delete frontend directory

--- frontend/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-  return (
-    <div style={{ padding: "10px", background: "#333", color: "white" }}>
-      <h3>ThesEase System</h3>
-    </div>
-  );
-};
-
-export default Navbar;
