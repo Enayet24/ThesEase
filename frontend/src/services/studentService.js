fce2a7b7540fa026d2e6ebcbe3cf4414ce30c3ae
Enayet24 <enayet.ali3@gmail.com>
2026-05-05 16:41:52 +0600

    Updated the student features

--- frontend/src/services/studentService.js
+++ frontend/src/services/studentService.js
@@ -1,17 +1,25 @@
 import api from './api';
 
-
 export const browseAdvisors = (params) =>
   api.get('/student/advisors', { params });
-
 
 export const getAdvisorDetails = (advisorId) =>
   api.get(`/student/advisors/${advisorId}`);
 
-
 export const getFilteredSlots = (params) =>
   api.get('/student/slots', { params });
 
-
 export const getStudentDashboard = () =>
   api.get('/dashboard/student');
+
+export const recommendAdvisors = () =>
+  api.get('/student/recommend');
+
+export const searchByExpertise = (params) =>
+  api.get('/student/search-expertise', { params });
+
+export const getBookingHistory = () =>
+  api.get('/student/booking-history');
+
+export const cancelBooking = (id) =>
+  api.delete(`/student/cancel/${id}`);
